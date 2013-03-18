5b023394bc47bf1848982c870ba0379f4f658426
Michał Soczyński <soczynski.michal@gmail.com>
2013-03-18 10:15:47 +0100

    add super class for user

--- Entity/User.php
+++ Entity/User.php
@@ -4,19 +4,12 @@ namespace Btn\UserBundle\Entity;
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
- * @ORM\Entity()
- * @ORM\Table(name="user")
+ * @ORM\MappedSuperclass
  */
 class User extends BaseUser
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-
     /**
      * @var text
      *
@@ -44,4 +37,73 @@ class User extends BaseUser
      * @ORM\Column(name="ip", type="string", nullable=true)
      */
     private $ip = null;
+
+    /**
+     * Set agent
+     *
+     * @param string $agent
+     * @return User
+     */
+    public function setAgent($agent)
+    {
+        $this->agent = $agent;
+
+        return $this;
+    }
+
+    /**
+     * Get agent
+     *
+     * @return string
+     */
+    public function getAgent()
+    {
+        return $this->agent;
+    }
+
+    /**
+     * Set browser
+     *
+     * @param string $browser
+     * @return User
+     */
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
+
+        return $this;
+    }
+
+    /**
+     * Get browser
+     *
+     * @return string
+     */
+    public function getBrowser()
+    {
+        return $this->browser;
+    }
+
+    /**
+     * Set ip
+     *
+     * @param string $ip
+     * @return User
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    /**
+     * Get ip
+     *
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
 }
